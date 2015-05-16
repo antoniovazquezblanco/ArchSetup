dda68b8ee377d47a0eb7799f834d455115bb2c93
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-05-16 13:39:45 +0000

    First implementation of EntryWidget

---
+++ Interface/EntryWidget.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of ArchSetup.
+#
+# ArchSetup is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ArchSetup is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ArchSetup.  If not, see <http://www.gnu.org/licenses/>.
+
+import curses
+import logging
+from Interface.Widget import Widget
+
+class EntryWidget(Widget):
+    def __init__(self, y, x, text, cols, callback):
+        super().__init__(y, x, 1, len(text))
+        self.text = text
+        self.cols = cols
+        self.callback = callback
+
+    def draw(self, window):
+        (posy, posx) = self.position()
+        if not self.ishighlighted():
+            window.addstr(posy, posx, self.text.center(self.cols))
+        else:
+            window.addstr(posy, posx, self.text.center(self.cols), curses.A_REVERSE | curses.A_UNDERLINE)
+
+    def event(self, event):
+        if event == ord('\n'):
+            return
+
+        self.text = self.text + chr(event)
+        self.callback("refresh")
