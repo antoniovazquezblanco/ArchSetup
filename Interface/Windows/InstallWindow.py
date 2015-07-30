374bb0af5fbba2cc941c477e930c629316438d84
Marius Messerschmidt <marius.messerschmidt@googlemail.com>
2015-07-30 15:45:48 +0200

    Fixed type Staus to Status
    it is now working, but the user gets no notifications + app freezes + prints weird messages ;-)
    
    Signed-off-by: Marius Messerschmidt <marius.messerschmidt@googlemail.com>

--- Interface/Windows/InstallWindow.py
+++ Interface/Windows/InstallWindow.py
@@ -39,7 +39,7 @@ class InstallWindow(SetupWindow):
         self.callback = callback
         self.addwidget(TextWidget(1, 1, _('Installing base system'),  40))
         self.status_label = TextWidget(0,0,"status", 40)
-        self.addwidget(ScrollWidget(4, 1, 40, 20, self.staus_label, self.event))
+        self.addwidget(ScrollWidget(4, 1, 40, 20, self.status_label, self.event))
         self.addwidget(SpacerWidget(23, 1, 1))
         self.setnextcallback(callback, '')
         self.setprevcallback(callback, 'prev')
